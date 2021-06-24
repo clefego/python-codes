b729a57c58bc2f412e5edc840b48264523d25a05
clefego <70776298+clefego@users.noreply.github.com>
2021-06-24 07:11:54 -0300

    teste4

--- tipo.py
+++ tipo.py
@@ -5,3 +5,6 @@ s1 = n1 + n2
 print('A soma entre', n1, 'e', n2, 'vale', s1)
 print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s1))
 print('O valor decimal vale: ', n3)
+
+print(type(n2))
+print(type(n3))
