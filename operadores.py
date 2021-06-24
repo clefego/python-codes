ceeda7757a60cc441e91740c1e5f39413e1c98d4
clefego <70776298+clefego@users.noreply.github.com>
2021-06-24 07:29:54 -0300

    Teste6

--- operadores.py
+++ operadores.py
@@ -6,4 +6,5 @@ d = n1 / n2
 di = n1 // n2
 e = n1 ** n2
 print('A soma vale {}'.format(s))
-print('A soma vale {}'.format(s))
+print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(s, m, d))
+print('Divisão inteira {} e a potência {}'.format(di, e))
