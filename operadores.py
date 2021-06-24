4eb6ef73af91ba891691e21622e0e1868ce61236
clefego <70776298+clefego@users.noreply.github.com>
2021-06-24 07:25:27 -0300

    teste5

---
+++ operadores.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o valor1: '))
+n2 = int(input('Digite o valor1: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma vale {}'.format(s))
+print('A soma vale {}'.format(s))
