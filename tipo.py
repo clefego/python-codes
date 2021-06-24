61b8660aa96ede20780bdf230c090547a2e83888
clefego <70776298+clefego@users.noreply.github.com>
2021-06-24 07:02:37 -0300

    teste3

--- tipo.py
+++ tipo.py
@@ -4,4 +4,4 @@ n3 = float(input('Digite o valor3: '))
 s1 = n1 + n2
 print('A soma entre', n1, 'e', n2, 'vale', s1)
 print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s1))
-print('O valor decimal vale: ', n4)
+print('O valor decimal vale: ', n3)
