ac46c7441a9f3939838a811bfa3b99ebdf6c015f
clefego <70776298+clefego@users.noreply.github.com>
2021-06-26 23:44:06 -0300

    Função

---
+++ funcao9.py
@@ -0,0 +1,9 @@
+def somar(a=0, b=0, c=0):
+    s = a + b + c 
+    return s
+
+r1 = somar(3, 12, 3)
+r2 = somar(3 ,4)
+r3 = somar(6)
+
+print(f'Os resultados das somas correspondem a {r1}, {r2} e {r3}')
